569692a18e2843134f21fe4f660d97566e557b0c
Erichain <jyamol520@gmail.com>
2016-01-09 14:34:06 +0800

    complete frame building
    Signed-off-by: Erichain <jyamol520@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,120 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
-var uglify = require('gulp-uglify');
-var uglify_html = require('gulp-minify-html');
+/**
+ * gulpfile
+ * used to build, test, etc.
+ * @author Erichain
+ * @date 2016-01-10
+ */
+'use strict';
 
-var paths = {
-    sass: ['./www/styles/**/*.scss']
-};
+/* packages */
+var gulp = require('gulp'),
+    compass = require('gulp-compass'),
+    wiredep = require('wiredep').stream,
+    cssnano = require('gulp-cssnano'),
+    livereload = require('gulp-livereload'),
+    rimraf = require('rimraf'),
+    runSequence = require('run-sequence'),
+    jshint = require('gulp-jshint'),
+    connect = require('gulp-connect'),
+    open = require('gulp-open'),
+    plumber = require('gulp-plumber'),
 
-gulp.task('default', ['sass']);
+/* define root app */
+    rootApp = {
+        app: require('./bower.json').appPath || './www',
+        dist: 'dist'
+    },
 
-gulp.task('sass', function(done) {
-    gulp.src('./www/styles/main.scss')
-        .pipe(sass())
-        .on('error', sass.logError)
-        .pipe(gulp.dest('./www/styles/'))
-        .pipe(minifyCss({
-            keepSpecialComments: 0
+/* files paths */
+    paths = {
+        scripts: [rootApp.app + '/scripts/**/*.js'],
+        styles: [rootApp.app + '/scss/**/*.scss'],
+        test: [rootApp.app + '/test/spec/**/*.js'],
+        testRequire: [
+            rootApp.app + '/vendor/angular/angular.js',
+            rootApp.app + '/vendor/angular-mocks/angular-mocks.js',
+            rootApp.app + '/vendor/angular-resource/angular-resource.js',
+            rootApp.app + '/vendor/angular-cookies/angular-cookies.js',
+            rootApp.app + '/vendor/angular-sanitize/angular-sanitize.js',
+            rootApp.app + '/vendor/angular-route/angular-route.js',
+            rootApp.app + '/test/mock/**/*.js',
+            rootApp.app + '/test/spec/**/*.js'
+        ],
+        karma: rootApp.app + '/karma.conf.js',
+        views: {
+            main: rootApp.app + '/index.html',
+            files: [rootApp.app + '/**/*.html']
+        },
+        devFiles: rootApp.app + '*.json'
+    };
+
+////////////////////////
+// Tasks definition ////
+////////////////////////
+
+gulp.task('compass', function () {
+    gulp.src(paths.styles)
+        .pipe(plumber())
+        .pipe(compass({
+            sass: rootApp.app + '/scss',
+            css: rootApp.app + '/styles',
+            image: rootApp.app + '/images'
         }))
-        .pipe(rename({
-            extname: '.min.css'
-        }))
-        .pipe(gulp.dest('.tmp'))
-        .on('end', done);
+        .pipe(cssnano())
+        .pipe(gulp.dest(rootApp.app + '/styles/'))
+        .pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
-    gulp.watch(paths.sass, ['sass']);
+gulp.task('lint:scripts', function () {
+    gulp.src(paths.scripts)
+        .pipe(plumber())
+        .pipe(jshint())
+        .pipe(connect.reload());
 });
 
-gulp.task('install', ['git-check'], function() {
-    return bower.commands.install()
-        .on('log', function(data) {
-            gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-        });
+gulp.task('html', function () {
+    gulp.src(paths.views.files)
+        .pipe(plumber())
+        .pipe(connect.reload());
 });
 
-gulp.task('compress',[], function () {
-    var opt = {};
-
-    gulp.src('./www/views/**/*.html')
-        .pipe(uglify_html())
-        .pipe(gulp.dest('./dist/views'));
+gulp.task('clean:css', function ( cb ) {
+    rimraf(rootApp.app + '/styles', cb)
 });
 
-gulp.task('build', ['compress'], function () {
-    gulp.src('./www/scripts/**/*.js')
-        .pipe(uglify())
-        .pipe(rename({
-            extname: '.min.js'
-        }))
-        .pipe(gulp.dest('./dist/scripts'));
+gulp.task('start:server', function () {
+    connect.server({
+        root: [rootApp.app],
+        livereload: {
+            port: 35733
+        },
+        port: 9011
+    });
 });
 
-gulp.task('git-check', function(done) {
-    if (!sh.which('git')) {
-        console.log(
-            '  ' + gutil.colors.red('Git is not installed.'),
-            '\n  Git, the version control system, is required to download Ionic.',
-            '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-            '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-        );
-        process.exit(1);
-    }
-    done();
+gulp.task('start:client', ['start:server', 'compass'], function () {
+    var options = {
+        uri: 'http://0.0.0.0:9011',
+        app: 'Google Chrome'
+    };
+
+    gulp.src(paths.views.main)
+        .pipe(plumber())
+        .pipe(open(options));
 });
+
+gulp.task('watch', function () {
+    gulp.watch(paths.styles, ['compass']);
+    gulp.watch(paths.views.files, ['html']);
+    gulp.watch(paths.scripts, ['lint:scripts']);
+});
+
+gulp.task('serve', function ( cb ) {
+    runSequence(
+        'clean:css',
+        ['lint:scripts'],
+        ['start:client'],
+        'watch', cb)
+});
+
+gulp.task('default', ['start:server', 'watch']);
